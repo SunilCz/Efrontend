f803ee83ee4bffd65ce241131cdcc84c3e8c09d6
SunilCz <sunil.chaudhary7281@gmai.com>
2023-05-30 21:46:25 +0545

    added sucess page navigate

--- pages/cart.js
+++ pages/cart.js
@@ -1,14 +1,16 @@
 import React, { useMemo, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useSelector } from "react-redux";
 import Wrapper from "@/components/Wrapper";
 import CartItem from "@/components/CartItem";
-import { useSelector } from "react-redux";
 import KhaltiCheckout from "khalti-checkout-web";
+import { useRouter } from "next/router";
 
 const Cart = () => {
   const [loading, setLoading] = useState(false);
   const { cartItems } = useSelector((state) => state.cart);
+  const router = useRouter();
 
   const subTotal = useMemo(() => {
     return cartItems.reduce((total, val) => total + val.attributes.price, 0);
@@ -22,24 +24,19 @@ const Cart = () => {
       productIdentity: "your_product_identity",
       productName: "Your Product Name",
       productUrl: "http://localhost:3000",
-      amount: subTotal * 100, // Convert the amount to the smallest currency unit (e.g., paisa for NPR),
+      amount: subTotal * 1,
       eventHandler: {
         onSuccess(payload) {
-          // Payment successful
           console.log(payload);
           setLoading(false);
-          // Perform necessary actions after successful payment
+          router.push("/success");
         },
         onError(error) {
-          // Payment error
           console.log(error);
           setLoading(false);
-          // Handle the error and show appropriate message to the user
         },
         onClose() {
-          // Payment window closed
           setLoading(false);
-          // Handle the event if the user closes the payment window without completing the payment
         },
       },
     };
@@ -53,29 +50,20 @@ const Cart = () => {
       <Wrapper>
         {cartItems.length > 0 && (
           <>
-            {/* HEADING AND PARAGRAPH START */}
             <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
               <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
                 Shopping Cart
               </div>
             </div>
-            {/* HEADING AND PARAGRAPH END */}
-
-            {/* CART CONTENT START */}
             <div className="flex flex-col lg:flex-row gap-12 py-10">
-              {/* CART ITEMS START */}
               <div className="flex-[2]">
                 <div className="text-lg font-bold">Cart Items</div>
                 {cartItems.map((item) => (
                   <CartItem key={item.id} data={item} />
                 ))}
               </div>
-              {/* CART ITEMS END */}
-
-              {/* SUMMARY START */}
               <div className="flex-[1]">
                 <div className="text-lg font-bold">Summary</div>
-
                 <div className="p-5 my-5 bg-black/[0.05] rounded-xl">
                   <div className="flex justify-between">
                     <div className="uppercase text-md md:text-lg font-medium text-black">
@@ -92,8 +80,6 @@ const Cart = () => {
                     transaction fees.
                   </div>
                 </div>
-
-                {/* BUTTON START */}
                 <button
                   className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
                   onClick={handlePayment}
@@ -101,15 +87,10 @@ const Cart = () => {
                   Checkout
                   {loading && <img src="/spinner.svg" />}
                 </button>
-                {/* BUTTON END */}
               </div>
-              {/* SUMMARY END */}
             </div>
-            {/* CART CONTENT END */}
           </>
         )}
-
-        {/* This is empty screen */}
         {cartItems.length < 1 && (
           <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
             <Image
